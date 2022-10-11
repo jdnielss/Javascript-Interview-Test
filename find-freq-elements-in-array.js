743ea25ea449c6b73a331c8d2962597d710766ec
Riski Makira <33960326+makiraid@users.noreply.github.com>
2022-10-11 21:52:08 +0700

    Create find-freq-elements-in-array.js

---
+++ find-freq-elements-in-array.js
@@ -0,0 +1,22 @@
+/**
+ * Find the frequency of elements in array
+ * let arr = ["hello", "world", "javascript", "hello", "javascript"]
+ *
+ * @param {string[]} words
+ *
+ * @returns {Object}
+ */
+
+function answer(words) {
+	return words.reduce((words, word) => {
+		if (word in words) {
+			words[word]++
+		} else {
+			words[word] = 1
+		}
+		return words
+	}, {});
+}
+
+let arr = ["hello", "world", "javascript", "hello", "javascript"]
+console.log(answer(arr))
